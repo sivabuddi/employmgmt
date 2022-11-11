cad67ee7c22a258a652ec1ffe67ff07e64f75b06
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-11-11 16:48:42 -0600

    GetMapping in REST

--- src/main/java/com/learn/employmgmt/EmploymgmtApplication.java
+++ src/main/java/com/learn/employmgmt/EmploymgmtApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmploymgmtApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmploymgmtApplication.class, args);
+		  SpringApplication.run(EmploymgmtApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/learn/employmgmt/Testing.java
@@ -0,0 +1,14 @@
+package com.learn.employmgmt;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Testing {
+
+    @GetMapping("/hai")
+    public String getMessage(){
+        return "getMessage() method from Testing Classes";
+    }
+
+}
